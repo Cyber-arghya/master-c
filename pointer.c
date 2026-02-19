20438fe8d99f9dc3f3eefb5c3cf57e8ff9a41958
Cyber-arghya <work.arghya01@gmail.com>
2026-02-20 00:58:48 +0600

    Updates

--- pointer.c
+++ pointer.c
@@ -8,17 +8,18 @@ int *ptr   = &age; // ptr is a pointer variable
 int **pptr = &ptr; // pptr is a pointer to pointer  variable
 
 //address
-printf("------------------------------------------------------------\n");       
+printf("------------------------------------------------------------\n");
 printf("Adress of age [ &age ]-------------p = %p\n", &age); 
 printf("Value  of age [ age ]--------------d = %d\n", age ); 
 printf("------------------------------------------------------------\n"); 
 printf("Adress inside [ ptr ] -------------p = %p\n", ptr );  // ptr stores the address of age   
 printf("Address of [ptr] (&ptr)------------p = %p\n", &ptr);  // ptr own adress 
-printf("Value at that adress [ *ptr ]------d = %d\n", *ptr);
+printf("Value at adress [ *ptr ]-----------d = %d\n", *ptr);
+printf("Value at adress [ *(&age) ]--------d = %d\n", *(&age));
 printf("------------------------------------------------------------\n");
 printf("Adress inside [ pptr ] ------------p = %p\n", pptr ); // pptr stores the address of ptr   
 printf("Address of [pptr] (&pptr)----------p = %p\n", &pptr); // pptr own adress
-printf("Value at that adress [ **pptr ]----d = %d\n", **pptr);// To see the data, you "hop" twice with **pptr.        
+printf("Value at adress [ **pptr ]---------d = %d\n", **pptr);// To see the data, you "hop" twice with **pptr.        
 printf("------------------------------------------------------------\n");
 return 0;
 }
